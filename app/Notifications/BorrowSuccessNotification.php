a866158054937ca33d58e85ce187a6736728ac07
ndahprtw <indhcnch@gmail.com>
2026-07-02 16:44:59 +0700

    [FEAT] implementasi queue

--- app/Notifications/BorrowSuccessNotification.php
+++ app/Notifications/BorrowSuccessNotification.php
@@ -4,11 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Borrow;
 use Illuminate\Bus\Queueable;
-// use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class BorrowSuccessNotification extends Notification
+class BorrowSuccessNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
